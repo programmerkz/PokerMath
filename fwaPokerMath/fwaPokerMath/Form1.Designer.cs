b7a4349ad92317d0d178c6c2835be2e60cbb7fe2
Alexandr Volossatov <sanya_666@mail.ru>
2019-04-25 12:09:33 +0600

    добавлены элементы управления формы для будущего сбора статистики

--- fwaPokerMath/fwaPokerMath/Form1.Designer.cs
+++ fwaPokerMath/fwaPokerMath/Form1.Designer.cs
@@ -36,13 +36,19 @@
             this.txtConsole = new System.Windows.Forms.TextBox();
             this.chkPrintException = new System.Windows.Forms.CheckBox();
             this.txtClearConsole = new System.Windows.Forms.Button();
+            this.lblCreateDice = new System.Windows.Forms.Label();
+            this.lblGetStatistic = new System.Windows.Forms.Label();
+            this.lblIterCount = new System.Windows.Forms.Label();
+            this.nudIterCount = new System.Windows.Forms.NumericUpDown();
+            this.btnGetStatistic = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.nudDiceNumber)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFaceNumber)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIterCount)).BeginInit();
             this.SuspendLayout();
             // 
             // btnCreateDice
             // 
-            this.btnCreateDice.Location = new System.Drawing.Point(15, 73);
+            this.btnCreateDice.Location = new System.Drawing.Point(15, 87);
             this.btnCreateDice.Name = "btnCreateDice";
             this.btnCreateDice.Size = new System.Drawing.Size(75, 23);
             this.btnCreateDice.TabIndex = 0;
@@ -52,9 +58,14 @@
             // 
             // nudDiceNumber
             // 
-            this.nudDiceNumber.Location = new System.Drawing.Point(125, 7);
+            this.nudDiceNumber.Location = new System.Drawing.Point(125, 29);
             this.nudDiceNumber.Maximum = new decimal(new int[] {
             255,
+            0,
+            0,
+            0});
+            this.nudDiceNumber.Minimum = new decimal(new int[] {
+            1,
             0,
             0,
             0});
@@ -70,7 +81,7 @@
             // lblDiceNumber
             // 
             this.lblDiceNumber.AutoSize = true;
-            this.lblDiceNumber.Location = new System.Drawing.Point(12, 9);
+            this.lblDiceNumber.Location = new System.Drawing.Point(12, 31);
             this.lblDiceNumber.Name = "lblDiceNumber";
             this.lblDiceNumber.Size = new System.Drawing.Size(107, 13);
             this.lblDiceNumber.TabIndex = 2;
@@ -79,7 +90,7 @@
             // lblFaceNumber
             // 
             this.lblFaceNumber.AutoSize = true;
-            this.lblFaceNumber.Location = new System.Drawing.Point(12, 35);
+            this.lblFaceNumber.Location = new System.Drawing.Point(12, 57);
             this.lblFaceNumber.Name = "lblFaceNumber";
             this.lblFaceNumber.Size = new System.Drawing.Size(107, 13);
             this.lblFaceNumber.TabIndex = 3;
@@ -87,9 +98,14 @@
             // 
             // nudFaceNumber
             // 
-            this.nudFaceNumber.Location = new System.Drawing.Point(125, 33);
+            this.nudFaceNumber.Location = new System.Drawing.Point(125, 55);
             this.nudFaceNumber.Maximum = new decimal(new int[] {
             255,
+            0,
+            0,
+            0});
+            this.nudFaceNumber.Minimum = new decimal(new int[] {
+            1,
             0,
             0,
             0});
@@ -130,11 +146,80 @@
             this.txtClearConsole.UseVisualStyleBackColor = true;
             this.txtClearConsole.Click += new System.EventHandler(this.txtClearConsole_Click);
             // 
+            // lblCreateDice
+            // 
+            this.lblCreateDice.AutoSize = true;
+            this.lblCreateDice.Location = new System.Drawing.Point(12, 10);
+            this.lblCreateDice.Name = "lblCreateDice";
+            this.lblCreateDice.Size = new System.Drawing.Size(123, 13);
+            this.lblCreateDice.TabIndex = 8;
+            this.lblCreateDice.Text = "Создать набор костей:";
+            // 
+            // lblGetStatistic
+            // 
+            this.lblGetStatistic.AutoSize = true;
+            this.lblGetStatistic.Location = new System.Drawing.Point(12, 152);
+            this.lblGetStatistic.Name = "lblGetStatistic";
+            this.lblGetStatistic.Size = new System.Drawing.Size(111, 13);
+            this.lblGetStatistic.TabIndex = 9;
+            this.lblGetStatistic.Text = "Собрать статистику:";
+            // 
+            // lblIterCount
+            // 
+            this.lblIterCount.AutoSize = true;
+            this.lblIterCount.Location = new System.Drawing.Point(12, 177);
+            this.lblIterCount.Name = "lblIterCount";
+            this.lblIterCount.Size = new System.Drawing.Size(59, 13);
+            this.lblIterCount.TabIndex = 10;
+            this.lblIterCount.Text = "Итераций:";
+            // 
+            // nudIterCount
+            // 
+            this.nudIterCount.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudIterCount.Location = new System.Drawing.Point(77, 175);
+            this.nudIterCount.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.nudIterCount.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudIterCount.Name = "nudIterCount";
+            this.nudIterCount.Size = new System.Drawing.Size(90, 20);
+            this.nudIterCount.TabIndex = 11;
+            this.nudIterCount.ThousandsSeparator = true;
+            this.nudIterCount.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // btnGetStatistic
+            // 
+            this.btnGetStatistic.Location = new System.Drawing.Point(12, 201);
+            this.btnGetStatistic.Name = "btnGetStatistic";
+            this.btnGetStatistic.Size = new System.Drawing.Size(75, 23);
+            this.btnGetStatistic.TabIndex = 12;
+            this.btnGetStatistic.Text = "Расчет";
+            this.btnGetStatistic.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(576, 364);
+            this.Controls.Add(this.btnGetStatistic);
+            this.Controls.Add(this.nudIterCount);
+            this.Controls.Add(this.lblIterCount);
+            this.Controls.Add(this.lblGetStatistic);
+            this.Controls.Add(this.lblCreateDice);
             this.Controls.Add(this.txtClearConsole);
             this.Controls.Add(this.chkPrintException);
             this.Controls.Add(this.txtConsole);
@@ -147,6 +232,7 @@
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.nudDiceNumber)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudFaceNumber)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudIterCount)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -162,6 +248,11 @@
         private System.Windows.Forms.TextBox txtConsole;
         private System.Windows.Forms.CheckBox chkPrintException;
         private System.Windows.Forms.Button txtClearConsole;
+        private System.Windows.Forms.Label lblCreateDice;
+        private System.Windows.Forms.Label lblGetStatistic;
+        private System.Windows.Forms.Label lblIterCount;
+        private System.Windows.Forms.NumericUpDown nudIterCount;
+        private System.Windows.Forms.Button btnGetStatistic;
     }
 }
 
