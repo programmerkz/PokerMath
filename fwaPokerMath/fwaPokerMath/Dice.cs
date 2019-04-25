a20b67a1eefea136c770faf730c12b9da159e015
Alexandr Volossatov <sanya_666@mail.ru>
2019-04-25 10:47:31 +0600

    добавлен класс Dice, свойства и конструктор

--- fwaPokerMath/fwaPokerMath/Dice.cs
+++ fwaPokerMath/fwaPokerMath/Dice.cs
@@ -30,5 +30,26 @@ namespace fwaPokerMath
 
     class Dice
     {
+        public byte DiceNumber { get; }
+        public byte FaceNumber { get; }
+        public List<SingleDice> Dices { get; }
+
+        public Dice(byte faceNumber, byte diceNumber)
+        {
+            if (faceNumber < 1) throw new ArgumentException("Количество граней на костях не может быть меньше 1", "faceNumber");
+            else if (diceNumber < 1) throw new ArgumentException("Количество костей не может быть меньше 1", "diceNumber");
+            else
+            {
+                DiceNumber = diceNumber;
+                FaceNumber = faceNumber;
+                Dices = new List<SingleDice>();
+
+                for (byte i = 0; i < DiceNumber; i++)
+                {
+                    SingleDice die = new SingleDice(FaceNumber);
+
+                }
+            }
+        }
     }
 }
