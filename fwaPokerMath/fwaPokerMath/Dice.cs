909cd586f927d2848c403543a98173ca7c7cc0cc
Alexandr Volossatov <sanya_666@mail.ru>
2019-04-25 10:28:26 +0600

    добавлено свойство faceNumber (количество граней), созданы два конструктора

--- fwaPokerMath/fwaPokerMath/Dice.cs
+++ fwaPokerMath/fwaPokerMath/Dice.cs
@@ -5,6 +5,23 @@ using System.Text;
 
 namespace fwaPokerMath
 {
+    class SingleDice
+    {
+        public byte FaceNumber { get; }
+
+        public SingleDice()
+        {
+            FaceNumber = 6;
+        }
+
+        public SingleDice(byte faceNumber)
+        {
+            if (faceNumber < 1) throw new ArgumentException("Количество граней на костях не может быть меньше 1", "faceNumber");
+            else
+                FaceNumber = faceNumber;
+        }
+    }
+
     class Dice
     {
     }
